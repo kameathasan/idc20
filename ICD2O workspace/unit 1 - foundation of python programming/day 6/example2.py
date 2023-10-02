a001376c67af946860e85a1964608568d9ee8918
kamea thasan <kthasan@bayviewglen.ca>
2023-10-02 14:52:04 -0400

    example 2 and 3

---
+++ ICD2O workspace/unit 1 - foundation of python programming/day 6/example2.py
@@ -0,0 +1,11 @@
+print("12345678901234567890")
+
+name = "steve"
+
+print(f"{name}")
+print(f"{name:>20}")
+print(f"{name:>10}")
+
+print(f"{name:<10}") #name is left aligned
+
+print(f"{name:^20}")
---
+++ ICD2O workspace/unit 1 - foundation of python programming/day 6/example3.py
@@ -0,0 +1,13 @@
+import math
+
+
+average = 67.4465456
+
+print(f"{average}")
+print(f"{average:.2f}")
+print(f"{average:>20}")
+print(f"{average:>20.2f}")
+#print(f"{average:.2f>20}") #does not work we need width/allignment first and decimal after
+
+print(math.pi)
+print(f"{math.pi:>10.2f}")
