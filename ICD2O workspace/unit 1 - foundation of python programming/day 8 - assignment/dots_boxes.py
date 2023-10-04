62aadb0d373392973839eab16d6e2fe7af3c5f4e
kamea thasan <kthasan@bayviewglen.ca>
2023-10-04 13:52:40 -0400

    dots and boxes on 10/4/2023

---
+++ ICD2O workspace/unit 1 - foundation of python programming/day 8 - assignment/dots_boxes.py
@@ -0,0 +1,51 @@
+player_name = input("Enter your name: ")
+
+print("")
+print("GAME #1: ")
+print("")
+opponent1 = input("Enter your opponents name: ")
+your_points1 = input("Enter your points:")
+opp_points1 = input("Enter your opponent's points: ")
+box_percent1 = your_points1/opp_points1
+
+print("")
+print("GAME #2: ")
+print("")
+opponent2 = input("Enter your opponents name: ")
+your_points2 = input("Enter your points:")
+opp_points2 = input("Enter your opponent's points: ")
+box_percent2 = your_points2/opp_points2
+
+print("")
+print("GAME #3: ")
+print("")
+opponent3 = input("Enter your opponents name: ")
+your_points3 = input("Enter your points:")
+opp_points3 = input("Enter your opponent's points: ")
+box_percent3 = your_points3/opp_points3
+
+print("")
+print("GAME #4: ")
+print("")
+opponent4 = input("Enter your opponents name: ")
+your_points4 = input("Enter your points:")
+opp_points4 = input("Enter your opponent's points: ")
+box_percent4 = your_points4/opp_points4
+
+print("")
+print("GAME #5: ")
+print("")
+opponent5 = input("Enter your opponents name: ")
+your_points5 = input("Enter your points:")
+opp_points5 = input("Enter your opponent's points: ")
+box_percent5 = your_points5/opp_points5
+
+print("")
+print("")
+print("Dots and Boxes Score Tracker")
+print(f"Players name: {player_name}")
+print("")
+print("")
+print(f"{'Opponent':<10} {'Your Points':>10} {'Opponent Points':>5} {'Box %':>8}")
+print(f"======================================================")
+print(f"{opponent1:<10} {your_points1:>15} {opp_points1:>10}")
